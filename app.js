e83579696add36f98c612ceda6cf05d445ccd8da
Sunny <kimsyng1120@gmail.com>
2022-05-06 14:57:02 +0900

    Form Submission: form내에서 버튼 클릭하거나 type이 submut인 input을 누르면 form이 submit 되고 새로고침 됨

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const loginBtn= document.querySelector(".login-form button");
 
 function onBtnClick(){
-  console.log(loginInput.value);
-  console.log("clicked!");
+  const value = loginInput.value;
+  console.log(value);
 }
 loginBtn.addEventListener("click", onBtnClick);
