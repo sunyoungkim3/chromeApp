b18ba0a06f60833c62afa1d5fbb4ee23150e2bc4
Sunny <kimsyng1120@gmail.com>
2022-05-06 16:14:04 +0900

    localItem 유무에 따라 보여지는 페이지 다르게 하기_Sunny

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("h1");
 
-function onHandleSubmit(event){
-  event.preventDefault();
-  const useName = loginInput.value;
-  console.log(event);
+const HIDDEN_CLASSNAME = "hidden";
+//문자열로 이루어진 변수만 포함하는 변수명은 대문자로 작성
+const USERNAME_KEY ="username"; // 2번이상 반복되는 문자열은 변수화 하여 사용하는것이 안전
+const existUserName = localStorage.getItem(USERNAME_KEY);
+
+
+function onHandleSubmit(event) {
+  event.preventDefault(); //브라우저의 기본 동작 멈추고 원하는 동작 가능
+  const userName = loginInput.value; //loginInput value를 userName이라는 변수에 저장
+  localStorage.setItem(USERNAME_KEY, userName); //Key, value 형태로 작성
+  loginForm.classList.toggle(HIDDEN_CLASSNAME); //loginForm의 class name에 hidden 포함.
+  greeting.innerText = `Hello ${userName}`; //비어있는 h1 태그를 불러다가 안에 내용 채워주기
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit",onHandleSubmit)
+if (existUserName === null) {
+  //localStorage.getItem("userName")에 value 값이 없으면 null 출력
+  loginForm.addEventListener("submit", onHandleSubmit);
+} else {
+  loginForm.classList.toggle(HIDDEN_CLASSNAME); //loginForm의 class name에 hidden 포함.
+  greeting.innerText = `Hello ${existUserName}`; //비어있는 h1 태그를 불러다가 안에 내용 채워주기
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-// 함수(): 브라우저가 강제로 실행시켜
-// 브라우저 onHandleSubmit 호출 (info): 브라우저 내 event 정보를 가져와서 함수 실행
-// 공간만 만들면, 방금 일어난 event에 대한 정보를 지닌 argument가 채워진다
+
+
+// **로직
+// if(existUserName === null){
+//   form 출력
+// }else{
+//   greeting 출력
+// }
