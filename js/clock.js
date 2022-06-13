8b0173f85987215109eed363e30dbb0e7916f02d
Sunny <kimsyng1120@gmail.com>
2022-06-13 20:52:38 +0900

    5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -3,8 +3,11 @@ const clock = document.querySelector("#clock");
 // intervals: 주기적으로 발생하는 event
 function getClock() {
   const date = new Date();
-  console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-  clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  console.log(`${hours}:${minutes}:${seconds}`);
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(getClock, 1000);
@@ -14,3 +17,7 @@ getClock();
 // timeout:
 
 // setTimeout(sayHello, 000);
+
+// "1".padStart(2,"0") : 1이라는 string 이 2보다 작을 때, 앞에 0을 추가한다.
+//  "hello".padStart(20,"x"): "xxxxxxxxxxxxxxhello"
+// "1".padEnd(2,"0") : 1이라는 string 이 2보다 작을 때, 0을 추가한다.
